19edc3572de92e9d87bd6d9a6d93648982638bb3
Tetiana Tse <tanchikqp@gmail.com>
2024-01-04 00:55:20 +0200

    new Route

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import MainPage from './components/UI/Pages/MainPage';
-import Episodes from './components/UI/Pages/Episodes';
-
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import MainPage from "./components/UI/Pages/MainPage";
+import Episodes from "./components/UI/Pages/Episodes";
 
 function App() {
   return (
     <BrowserRouter>
-        <Routes>
-            <Route path="/" element={<MainPage />}/>
-            <Route path="/episodes" element={<Episodes />}/>
-        </Routes>
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+        <Route path="/episodes" element={<Episodes />} />
+        <Route path="/details" element={<Episodes />} />
+      </Routes>
     </BrowserRouter>
   );
 }
---
+++ src/components/UI/Pages/Details.jsx
@@ -0,0 +1,12 @@
+import MainPage from "./MainPage";
+import styles from './Details.module.scss';
+
+function Details() {
+    return ( 
+        <div className={styles.details}>
+            <MainPage/>
+        </div>
+     );
+}
+
+export default Details;
